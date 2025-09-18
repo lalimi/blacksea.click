ca6dbce159ced5ed4cd0e8394d79bc7116453f2a
lali.mi <lo.koteyka@gmail.com>
2025-09-18 13:49:58 +0300

    fix: remove sidekiq-pro dependencies and conflicts
    - Remove sidekiq-pro require statements from sidekiq initializer
    - Remove Sidekiq::Pro conditional checks and features
    - Clean up Gemfile.lock to resolve bundler conflicts
    - Reinstall dependencies without sidekiq-pro conflicts
    
    This resolves the bundler conflict between regular sidekiq and sidekiq-pro when no license is available.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,26 +1,9 @@
 # frozen_string_literal: true
 
-# `sidekiq-pro` is now only autoloaded in staging/production due to the Gemfile
-# group. This ensures `sidekiq-pro` is correctly loaded in all environments
-# when available.
-begin
-  require "sidekiq-pro"
-rescue LoadError
-  warn "sidekiq-pro is not installed"
-end
-
 require Rails.root.join("lib", "extras", "sidekiq_makara_reset_context_middleware")
 
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{ENV["SIDEKIQ_REDIS_HOST"]}" }
-
-  if defined?(Sidekiq::Pro)
-    # https://github.com/mperham/sidekiq/wiki/Reliability#using-super_fetch
-    config.super_fetch!
-
-    # https://github.com/mperham/sidekiq/wiki/Reliability#scheduler
-    config.reliable_scheduler!
-  end
 
   # Cleanup Dead Locks
   # https://github.com/mhenrixon/sidekiq-unique-jobs/tree/ec69ac93afccd56cd424e2a9738e5ed478d941b2#cleanup-dead-locks
@@ -55,8 +38,7 @@ SidekiqUniqueJobs.configure do |config|
   config.enabled = !Rails.env.test?
 end
 
-# https://github.com/mperham/sidekiq/wiki/Pro-Reliability-Client
-Sidekiq::Client.reliable_push! if defined?(Sidekiq::Pro) && !Rails.env.test?
+# Note: Sidekiq Pro features are not available without a license
 
 # Store exception backtrace
 # https://github.com/mperham/sidekiq/wiki/Error-Handling#backtrace-logging
