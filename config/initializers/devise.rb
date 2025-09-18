ebdf2ab1341023075ac5612bba82a4c8b1532a3c
lali.mi <lo.koteyka@gmail.com>
2025-09-18 21:15:26 +0300

    Comment out omniauth-twitter require for production

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require "omniauth-facebook"
-require "omniauth-twitter"
 require "omniauth-google-oauth2"
+# require "omniauth-twitter" # Commented out for production deployment
 
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
