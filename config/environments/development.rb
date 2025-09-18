1cbb153c0a86dda21f292d9c5b45a09c100b7a09
lali.mi <lo.koteyka@gmail.com>
2025-09-18 21:38:39 +0300

    Comment out Bullet N+1 query detection for production deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,12 +82,12 @@ Rails.application.configure do
 
   config.action_controller.raise_on_missing_callback_actions = true
 
-  # Bullet configuration for N+1 query detection
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.rails_logger = true
-    Bullet.add_footer = true
-  end
+  # Bullet configuration for N+1 query detection (disabled for production deployment)
+  # config.after_initialize do
+  #   Bullet.enable = true
+  #   Bullet.alert = true
+  #   Bullet.bullet_logger = true
+  #   Bullet.rails_logger = true
+  #   Bullet.add_footer = true
+  # end
 end
