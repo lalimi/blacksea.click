d07ed0450e776b2be3db3a107a300cad1ae11171
lali.mi <lo.koteyka@gmail.com>
2025-09-18 21:13:21 +0300

    Add check for ActiveRecordQueryTrace existence

--- config/initializers/active_record_query_trace.rb
+++ config/initializers/active_record_query_trace.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-if Rails.env.development?
+if Rails.env.development? && defined?(ActiveRecordQueryTrace)
   ActiveRecordQueryTrace.enabled = (ENV["QUERY_TRACE"] == "1")
 end
