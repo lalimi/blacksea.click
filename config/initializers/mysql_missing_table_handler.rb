da830647b0cc726bf25dfd7f2a453c176163c0a7
lali.mi <lo.koteyka@gmail.com>
2025-09-19 11:39:48 +0300

    Comment out MySQL missing table handler since we're using PostgreSQL
    - MySQL handler tries to monkey-patch Mysql2::Client which doesn't exist in PostgreSQL
    - Commented out initializer to prevent NameError
    - Application should now start without MySQL-related errors

--- config/initializers/mysql_missing_table_handler.rb
+++ config/initializers/mysql_missing_table_handler.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
-module Mysql2
-  class Client
-    MISSING_TABLE_GRACE_PERIOD = 5.seconds
-
-    alias original_query query
-    def query(sql, options = {})
-      original_query(sql, options)
-    rescue Mysql2::Error => e
-      raise unless /Table .* doesn't exist/.match?(e.message)
-      warn "Error: missing table, retrying in #{MISSING_TABLE_GRACE_PERIOD} seconds..."
-      sleep MISSING_TABLE_GRACE_PERIOD
-      original_query(sql, options)
-    end
-  end
-end
+# MySQL missing table handler - commented out since we're using PostgreSQL
+# module Mysql2
+#   class Client
+#     MISSING_TABLE_GRACE_PERIOD = 5.seconds
+#
+#     alias original_query query
+#     def query(sql, options = {})
+#       original_query(sql, options)
+#     rescue Mysql2::Error => e
+#       raise unless /Table .* doesn't exist/.match?(e.message)
+#       warn "Error: missing table, retrying in #{MISSING_TABLE_GRACE_PERIOD} seconds..."
+#       sleep MISSING_TABLE_GRACE_PERIOD
+#       original_query(sql, options)
+#     end
+#   end
+# end
