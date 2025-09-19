92821758d9ebb806a5eee37fef0040d5356dcc46
lali.mi <lo.koteyka@gmail.com>
2025-09-19 09:37:51 +0300

    Fix webpack ProvidePlugin path to ../utils/routes

--- config/webpack/common.js
+++ config/webpack/common.js
@@ -187,7 +187,7 @@ const config = {
   plugins: [
     webpackAssetsManifestPlugin,
     miniCssExtractPlugin,
-    new webpack.ProvidePlugin({ Routes: path.join("..", "..", "utils", "routes") }),
+    new webpack.ProvidePlugin({ Routes: path.join("..", "utils", "routes") }),
     new ForkTsCheckerWebpackPlugin({
       typescript: {
         configFile: path.join(rootPath, "tsconfig.json"),
