e373d16e79116c547166a2b7fe9a1fcb3c620116
lali.mi <lo.koteyka@gmail.com>
2025-09-19 09:24:03 +0300

    Enable Facebook OAuth authentication via Supabase

--- config/initializers/000_facebook.rb
+++ config/initializers/000_facebook.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# Facebook integration disabled for BlackSea
-FACEBOOK_APP_ID = ENV.fetch("FACEBOOK_APP_ID", "")
-FACEBOOK_APP_SECRET = ENV.fetch("FACEBOOK_APP_SECRET", "")
+# Facebook integration for BlackSea via Supabase
+FACEBOOK_APP_ID = ENV.fetch("FACEBOOK_APP_ID", "749801654538399")
+FACEBOOK_APP_SECRET = ENV.fetch("FACEBOOK_APP_SECRET", "2a8f63399f846f5a6fdf51673d008f5f")
 # FACEBOOK_OG_NAMESPACE = GlobalConfig.get("FACEBOOK_OG_NAMESPACE")
 # FACEBOOK_API_VERSION = GlobalConfig.get("FACEBOOK_API_VERSION", "v19.0")
