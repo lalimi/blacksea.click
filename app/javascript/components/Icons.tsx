ba5e6a797b23c26ec202f0aa50bb95acc2aa107e
lali.mi <lo.koteyka@gmail.com>
2025-09-19 10:50:16 +0300

    Add IconName type definition with all available icons

--- app/javascript/components/Icons.tsx
+++ app/javascript/components/Icons.tsx
@@ -1,6 +1,39 @@
 import cx from "classnames";
 import * as React from "react";
 
+export type IconName =
+  | "default"
+  | "fonts"
+  | "h1"
+  | "h2"
+  | "file-arrow-down"
+  | "file-music"
+  | "file-play"
+  | "file-text"
+  | "outline-key"
+  | "outline-cheveron-right"
+  | "outline-cheveron-down"
+  | "solid-check-circle"
+  | "chevron-right"
+  | "chevron-left"
+  | "chevron-down"
+  | "arrow-right"
+  | "arrow-left"
+  | "close"
+  | "menu"
+  | "search"
+  | "user"
+  | "settings"
+  | "home"
+  | "dashboard"
+  | "products"
+  | "orders"
+  | "analytics"
+  | "customers"
+  | "affiliates"
+  | "help"
+  | "logout";
+
 type IconProps = {
   name: IconName;
 } & React.JSX.IntrinsicElements["span"];
